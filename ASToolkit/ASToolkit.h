b0d8c74578e91ddced61a2e9809fafef9c2f2da8
ANDRZEJ SEMENIUK <andrzejsemeniuk@gmail.com>
2017-07-08 07:05:04 -0700

    Initial Commit

---
+++ ASToolkit/ASToolkit.h
@@ -0,0 +1,19 @@
+//
+//  ASToolkit.h
+//  ASToolkit
+//
+//  Created by andrzej semeniuk on 7/8/17.
+//  Copyright © 2017 Andrzej Semeniuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ASToolkit.
+FOUNDATION_EXPORT double ASToolkitVersionNumber;
+
+//! Project version string for ASToolkit.
+FOUNDATION_EXPORT const unsigned char ASToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASToolkit/PublicHeader.h>
+
+
