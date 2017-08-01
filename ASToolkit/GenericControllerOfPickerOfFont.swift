3fed962260711d8a0b7ed6fede124539baf88ff6
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2017-08-01 12:03:50 -0700

    added GenericController classes from Tweeper project

---
+++ ASToolkit/GenericControllerOfPickerOfFont.swift
@@ -0,0 +1,127 @@
+//
+//  GenericControllerOfFonts
+//  productGroceries
+//
+//  Created by Andrzej Semeniuk on 3/25/16.
+//  Copyright © 2017 Andrzej Semeniuk. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+open class GenericControllerOfPickerOfFont : UITableViewController
+{
+    public var names            : [String]          = []
+    
+    public var selected         : String            = ""
+    
+    
+    
+    override open func viewDidLoad()
+    {
+        tableView.dataSource        = self
+        
+        tableView.delegate          = self
+        
+        tableView.separatorStyle    = .none
+
+        
+        names = []
+        
+        for family in UIFont.familyNames {
+//            names.append(family)
+            for font in UIFont.fontNames(forFamilyName: family) {
+                print("family \(family), font \(font)")
+                names.append(font)
+            }
+        }
+        
+        print("font names:\(names)")
+        
+        reload()
+        
+        
+        super.viewDidLoad()
+    }
+    
+    override open func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        
+        // TODO clear data and reload table
+    }
+    
+    
+    
+    
+    
+    override open func numberOfSections      (in: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    override open func tableView             (_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return names.count
+    }
+    
+    override open func tableView             (_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let name = names[indexPath.row]
+        
+        let cell = UITableViewCell(style:.default,reuseIdentifier:nil)
+        
+        if let label = cell.textLabel {
+            label.text          = name
+            //            label.textColor = UIColor.grayColor()
+            label.font          = UIFont(name:name,size:UIFont.labelFontSize)
+            label.textAlignment = .left
+        }
+        
+        cell.selectionStyle = .default
+        cell.accessoryType  = name == selected ? .checkmark : .none
+        
+        return cell
+    }
+    
+    
+    
+    
+    open func reload()
+    {
+        names = names.sorted()
+        
+        tableView.reloadData()
+    }
+    
+    
+    
+    override open func viewWillAppear(_ animated: Bool)
+    {
+        reload()
+        
+        if let row = names.index(of: selected) {
+            let path = IndexPath(row:row,section:0)
+            tableView.scrollToRow(at: path as IndexPath,at:.middle,animated:true)
+        }
+        
+        super.viewWillAppear(animated)
+    }
+    
+    
+    
+    var update: (() -> ()) = {}
+    
+    override open func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        selected = names[indexPath.row]
+        
+        reload()
+        
+        update()
+    }
+    
+}
+
+// TODO: ADD FILTERING CAPABILITY + INTEGRATION WITH NAVIGATION CONTROLLER
+
---
+++ ASToolkit/GenericManagerOfSettings.swift
@@ -0,0 +1,70 @@
+//
+//  GenericManagerOfSettings.swift
+//  productTweeper
+//
+//  Created by andrzej semeniuk on 7/29/17.
+//  Copyright © 2017 Andrzej Semeniuk. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public protocol GenericManagerOfSettings : class {
+    func synchronize    ()
+    func encode         (data:inout [String:Any], withPrefix prefix:String?, withSuffix suffix:String?)
+    func decode         (data:[String:Any], withPrefix prefix:String?, withSuffix suffix:String?)
+    func reset          (withPrefix prefix:String?, withSuffix suffix:String?)
+}
+
+extension GenericManagerOfSettings {
+    
+    public func encode(data:inout [String:Any], withPrefix prefix:String? = nil, withSuffix suffix:String? = nil) {
+        for child in Mirror(reflecting: self).children {
+            if let label = child.label {
+                if let prefix = prefix, !label.hasPrefix(prefix) {
+                    continue
+                }
+                if let suffix = suffix, !label.hasSuffix(suffix) {
+                    continue
+                }
+            }
+            if let setting = child.value as? ToDictionary {
+                setting.to(dictionary:&data)
+            }
+        }
+    }
+    
+    public func decode(data:[String:Any], withPrefix prefix:String? = nil, withSuffix suffix:String? = nil) {
+        for child in Mirror(reflecting: self).children {
+            if let label = child.label {
+                if let prefix = prefix, !label.hasPrefix(prefix) {
+                    continue
+                }
+                if let suffix = suffix, !label.hasSuffix(suffix) {
+                    continue
+                }
+            }
+            if let setting = child.value as? FromDictionary {
+                setting.from(dictionary:data)
+            }
+        }
+    }
+    
+    public func reset(withPrefix prefix:String? = nil, withSuffix suffix:String? = nil) {
+        for child in Mirror(reflecting: self).children {
+            if let label = child.label {
+                if let prefix = prefix, !label.hasPrefix(prefix) {
+                    continue
+                }
+                if let suffix = suffix, !label.hasSuffix(suffix) {
+                    continue
+                }
+            }
+            if let setting = child.value as? Resettable {
+                setting.reset()
+            }
+        }
+    }
+    
+}
+
