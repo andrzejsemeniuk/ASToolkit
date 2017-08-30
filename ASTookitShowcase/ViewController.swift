49801e88ed3993ca2a4ac069c1a840d46bea6a8c
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2017-08-29 18:52:07 -0700

    added color picker test code

--- ASTookitShowcase/ViewController.swift
+++ ASTookitShowcase/ViewController.swift
@@ -7,12 +7,69 @@
 //
 
 import UIKit
+import ASToolkit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if false {
+            let picker = GenericControllerOfPickerOfColor()
+            picker.rowHeight = 80
+            let colors:[[UIColor]] = [
+                [.red,.orange,.yellow],
+                [.blue,.green,.aqua],
+                [.white],
+                [.red,.orange,.yellow],
+                [.blue,.green,.aqua],
+                [.white,.lightGray,.gray,.darkGray,.black],
+                [.red,.orange,.yellow],
+                [.blue,.green,.aqua],
+                [.white,.lightGray,.gray,.darkGray,.black],
+                [.red,.orange,.yellow],
+                [.blue,.green,.aqua],
+                [.white,.lightGray,.gray,.darkGray,.black],
+                [.red,.orange,.yellow],
+                [.blue,.green,.aqua],
+                [.white,.lightGray,.gray,.darkGray,.black],
+                [.red,.orange,.yellow],
+                [.blue,.green,.aqua],
+                [.white,.lightGray,.gray,.darkGray,.black],
+                [.red,.orange,.yellow],
+                [.blue,.green,.aqua],
+                [.gray],
+                ]
+            picker.flavor = .matrixOfSolidCircles(selected: .white, colors: colors, diameter: 60, space: 16)
+        }
+        else if true {
+            
+            let picker = PickerOfColorWithSliders.create(withComponents: [
+                .colorDisplay           (height:64,kind:.dot),
+                //                .sliderRed              (height:16),
+                //                .sliderGreen            (height:16),
+                //                .sliderBlue             (height:16),
+                .sliderAlpha            (height:32),
+                .sliderHue              (height:16),
+                .sliderSaturation       (height:16),
+                .sliderBrightness       (height:16),
+                //                .sliderCyan             (height:32),
+                //                .sliderMagenta          (height:32),
+                //                .sliderYellow           (height:32),
+                //                .sliderKey              (height:32),
+                .storageDots            (radius:16, columns:6, rows:3, colors:[.white,.gray,.black,.orange,.red,.blue,.green,.yellow])
+                ])
+            picker.frame = UIScreen.main.bounds
+            self.view = picker
+            picker.backgroundColor = UIColor(white:0.95)
+            picker.handler = { color in
+                print("new color\(color)")
+            }
+            picker.set(color:UIColor(rgb:[0.64,0.13,0.78]), animated:true)
+        }
+
+        
     }
 
     override func didReceiveMemoryWarning() {
