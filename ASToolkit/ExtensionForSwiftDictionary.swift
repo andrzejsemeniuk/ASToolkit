3072569edfb34aad450e04fe0ec5624f46b56f5d
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2020-02-06 20:24:23 -0800

    work on symbols table

--- ASToolkit/ExtensionForSwiftDictionary.swift
+++ ASToolkit/ExtensionForSwiftDictionary.swift
@@ -8,19 +8,25 @@
 
 import Foundation
 
-extension Dictionary {
+extension Dictionary where Key == String, Value == String {
 
 	public func json(options: JSONSerialization.WritingOptions = [.prettyPrinted]) -> String? {
-		guard let jsonData = try? JSONSerialization.data(withJSONObject: self, options: options) else {
-			return nil
-		}
-		return String(data: jsonData, encoding: .utf8)
+//		guard let jsonData = try? JSONSerialization.data(withJSONObject: self, options: options) else {
+//			return nil
+//		}
+//		return String(data: jsonData, encoding: .utf8)
+        
+        guard let encoded = try? JSONEncoder().encode(self) else {
+            return nil
+        }
+        return String(data: encoded, encoding: .utf8)
 	}
 
-	static public func from(json text: String, options: JSONSerialization.ReadingOptions = []) -> [String: Any]? {
+	static public func from(json text: String, options: JSONSerialization.ReadingOptions = []) -> [String: String]? {
 		if let data = text.data(using: .utf8) {
 			do {
-				return try JSONSerialization.jsonObject(with: data, options: options) as? [String: Any]
+//				return try JSONSerialization.jsonObject(with: data, options: options) as? [String: Any]
+                return try JSONDecoder().decode(Dictionary<String,String>.self, from: data)
 			}
 			catch {
 
@@ -28,5 +34,7 @@ extension Dictionary {
 		}
 		return nil
 	}
+    
+    
 
 }
