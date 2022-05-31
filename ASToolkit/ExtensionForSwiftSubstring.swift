006cca5da92cdbb4986eace21e104150433534d4
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2022-05-31 11:59:42 -0400

    added conversions to numeric values for Substring

--- ASToolkit/ExtensionForSwiftSubstring.swift
+++ ASToolkit/ExtensionForSwiftSubstring.swift
@@ -8,10 +8,21 @@
 
 import Foundation
 
-extension Substring
+public extension Substring
 {
-	public var string : String {
+	var string : String {
 		return String(self)
 	}
+}
+
+public extension Substring {
+    
+    var  asDouble  :  Double?  { Double(self) }
+    var  asCGFloat :  CGFloat? { CGFloat(self.asString) }
+    var  asInt     :  Int?     { Int(self) }
+    var  asUInt    :  UInt?    { UInt(self) }
+    var  asUInt64  :  UInt64?  { UInt64(self.replacingOccurrences(of:  ",",  with:  "")) }
+    var  asFloat   :  Float?   { Float(self.asString) }
+
 }
 
