f1bbe2fdf6d63a18c305e53eb94370c30e8fe032
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2022-10-05 20:44:35 -0400

    work annotation highlight rendering

--- ASToolkit/Weak.swift
+++ ASToolkit/Weak.swift
@@ -21,9 +21,9 @@ open class Weak<T: AnyObject> {
 public typealias WeakRef = Weak
 
 
-open class WeakEquatable<T: AnyObject & Equatable> : Weak<T> {
+open class WeakEquatable<T: AnyObject & Equatable> : Weak<T>, Equatable {
     
-    public static func == (lhs: WeakEquatable, rhs: WeakEquatable) -> Bool {
+    public static func == (lhs: WeakEquatable<T>, rhs: WeakEquatable<T>) -> Bool {
         return lhs.value != nil && rhs.value != nil && lhs.value! == rhs.value!
     }
 }
@@ -33,8 +33,8 @@ public typealias WeakEquatableRef = WeakEquatable
 
 open class WeakHashable<T: AnyObject & Hashable> : WeakEquatable<T>, Hashable {
     
-    public var hashValue : Int {
-        return value?.hashValue ?? 0
+    public func hash(into hasher: inout Hasher) {
+        value?.hash(into: &hasher)
     }
     
     public static func == (lhs: WeakHashable<T>, rhs: WeakHashable<T>) -> Bool {
