55f52f071f591798f856297a3e0897ebecceef5f
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2023-02-07 15:34:20 -0500

    work on chart annotation hover with distance to mouse move location

--- ASToolkit/ExtensionForCoreGraphics.swift
+++ ASToolkit/ExtensionForCoreGraphics.swift
@@ -503,6 +503,35 @@ public extension CGPoint
     func distance               (to: CGPoint) -> CGFloat { (to-self).length }
     func distanceSquared        (to: CGPoint) -> CGFloat { (to-self).lengthSquared }
     
+    func distanceToSegment(_ v: CGPoint, _ w: CGPoint) -> CGFloat {
+        let pv_dx = x - v.x
+        let pv_dy = y - v.y
+        let wv_dx = w.x - v.x
+        let wv_dy = w.y - v.y
+
+        let dot = pv_dx * wv_dx + pv_dy * wv_dy
+        let len_sq = wv_dx * wv_dx + wv_dy * wv_dy
+        let param = dot / len_sq
+
+        var int_x, int_y: CGFloat /* intersection of normal to vw that goes through p */
+
+        if param < 0 || (v.x == w.x && v.y == w.y) {
+            int_x = v.x
+            int_y = v.y
+        } else if param > 1 {
+            int_x = w.x
+            int_y = w.y
+        } else {
+            int_x = v.x + param * wv_dx
+            int_y = v.y + param * wv_dy
+        }
+
+        /* Components of normal */
+        let dx = x - int_x
+        let dy = y - int_y
+
+        return sqrt(dx * dx + dy * dy)
+    }
     
     func isLeft                 (of: CGPoint) -> Bool   { x < of.x }
     func isRight                (of: CGPoint) -> Bool   { x > of.x }
