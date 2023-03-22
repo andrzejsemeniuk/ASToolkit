0756498c3600aac11d80ac0e50fcb1d1957c1d24
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2023-03-22 12:12:56 -0400

    work on rendering annotation labels for trend line

--- ASToolkit/ExtensionForSpriteKit.swift
+++ ASToolkit/ExtensionForSpriteKit.swift
@@ -21,8 +21,8 @@ import CoreImage
 
 extension SKNode
 {
-    @objc var width   :CGFloat                                                                    { return frame.size.width }
-    @objc var height  :CGFloat                                                                    { return frame.size.height }
+    @objc var width   :CGFloat                                                                    { return calculateAccumulatedFrame().width } // frame.size.width }
+    @objc var height  :CGFloat                                                                    { return calculateAccumulatedFrame().height } //frame.size.height }
     
     public func hFrom                  (ratio:CGFloat)                                 -> CGFloat  { return self.width * ratio }
     public func vFrom                  (ratio:CGFloat)                                 -> CGFloat  { return self.height * ratio }
