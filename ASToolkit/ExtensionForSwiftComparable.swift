100b9c6e1bbb57847c9da2fd94ee9f410f166556
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2023-04-05 15:46:38 -0400

    tweaks

--- ASToolkit/ExtensionForSwiftComparable.swift
+++ ASToolkit/ExtensionForSwiftComparable.swift
@@ -35,14 +35,45 @@ public extension Comparable {
 
 func comparing2<A: Comparable, B: Comparable>(_ first: (A,A), _ second: (B,B)) -> Bool {
     if first.0 < first.1 { return true }
-    if first.0 == first.1, second.0 < second.1 { return true }
-    return false
+    return first.0 == first.1 && second.0 < second.1
 }
 
 func comparing3<A: Comparable, B: Comparable, C: Comparable>(_ first: (A,A), _ second: (B,B), _ third: (C,C)) -> Bool {
-    comparing2(first,second) || third.0 < third.1
+    if first.0 < first.1 { return true }
+    if first.0 == first.1 {
+        if second.0 < second.1 { return true }
+        return second.0 == second.1 && third.0 < third.1
+    }
+    return false
 }
 
 func comparing4<A: Comparable, B: Comparable, C: Comparable, D: Comparable>(_ first: (A,A), _ second: (B,B), _ third: (C,C), _ fourth: (D,D)) -> Bool {
-    comparing3(first,second,third) || fourth.0 < fourth.1
+    if first.0 < first.1 { return true }
+    if first.0 == first.1 {
+        if second.0 < second.1 { return true }
+        if second.0 == second.1 {
+            if third.0 < third.1 { return true }
+            return third.0 == third.1 && fourth.0 < fourth.1
+        }
+    }
+    return false
 }
+
+func comparingUsingPaths<TYPE,FIRST: Comparable>(_ a: TYPE, _ b: TYPE, _ path1: KeyPath<TYPE,FIRST>) -> Bool {
+    a[keyPath: path1] < b[keyPath: path1]
+}
+
+func comparingUsingPaths<TYPE,FIRST: Comparable,SECOND: Comparable>(_ a: TYPE, _ b: TYPE, _ path1: KeyPath<TYPE,FIRST>, _ path2: KeyPath<TYPE,SECOND>) -> Bool {
+    if a[keyPath: path1] < b[keyPath: path1] { return true }
+    return a[keyPath: path1] == b[keyPath: path1] && a[keyPath: path2] < b[keyPath: path2]
+}
+
+func comparingUsingPaths<TYPE,FIRST: Comparable, SECOND: Comparable, THIRD: Comparable>(_ a: TYPE, _ b: TYPE, _ path1: KeyPath<TYPE,FIRST>, _ path2: KeyPath<TYPE,SECOND>, _ path3: KeyPath<TYPE,THIRD>) -> Bool {
+    if a[keyPath: path1] < b[keyPath: path1] { return true }
+    if a[keyPath: path1] == b[keyPath: path1] {
+        if a[keyPath: path2] < b[keyPath: path2] { return true }
+        return a[keyPath: path2] == b[keyPath: path2] && a[keyPath: path3] < b[keyPath: path3]
+    }
+    return false
+}
+
