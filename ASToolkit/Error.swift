ba32dfa44bdcd52cb1069ea431cecce6802abbb3
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2023-08-23 12:27:41 -0400

    refactoring DATA adding ASideViewKit.ViewForTree prototype renaming ASharkeeForMacApp into AppSharkeeForMacMain

---
+++ ASToolkit/Error.swift
@@ -0,0 +1,20 @@
+//
+//  Error.swift
+//  AppSharkeeForMac
+//
+//  Created by andrzej semeniuk on 8/22/23.
+//
+
+import Foundation
+
+public enum AnError : Swift.Error {
+    case isInvalidState(String)
+    case isNil(String)
+    case etc(String)
+    
+    case valueIsNil
+    case decoding, encoding
+    case notImplemented
+    case programmingError
+
+}
--- ASToolkit/ExtensionForSpriteKit.swift
+++ ASToolkit/ExtensionForSpriteKit.swift
@@ -444,11 +444,15 @@ public extension SKNode {
     
     func positionChildrenVertically(spacing: CGFloat, offset: CGFloat = 0) {
         let children = children
+        
         guard children.isNotEmpty else { return }
         
         let HEIGHTS = children.map { $0.calculateAccumulatedFrame().height }
+        
         var HEIGHT : CGFloat = HEIGHTS.reduce(0.0, { $0 + $1 }) + spacing * (children.count-1).asCGFloat
+        
         var y : CGFloat = -HEIGHT/2.0 + offset
+        
         for (i,child) in children.enumerated() {
             y += HEIGHTS[i]/2
             child.position.y = y
