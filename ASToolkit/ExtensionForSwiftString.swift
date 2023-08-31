229a792acfa5205e5af660abf5a6cafd1d40b065
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2023-08-31 07:52:32 -0400

    rearchitecting results info and table mechanics

--- ASToolkit/ExtensionForSwiftString.swift
+++ ASToolkit/ExtensionForSwiftString.swift
@@ -539,6 +539,9 @@ public extension Array where Element == String {
     var joinedByComma : String {
         self.joined(separator: ",")
     }
+    var joinedByDot : String {
+        self.joined(separator: ".")
+    }
     var joinedBySlash : String {
         self.joined(separator: "/")
     }
