c76476e01ea9a318f09e58b8b4377e64a7cdfa95
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2024-09-27 11:38:42 -0400

    work on ChatGPT

---
+++ ASToolkit/ExtensionForSwiftCodable.swift
@@ -0,0 +1,9 @@
+//
+//  ExtensionForSwiftCodable.swift
+//  ASToolkit
+//
+//  Created by andrzej semeniuk on 2024-09-26.
+//  Copyright © 2024 Andrzej Semeniuk. All rights reserved.
+//
+
+import Foundation
