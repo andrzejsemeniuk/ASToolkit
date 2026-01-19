0092eae029ab72e6976eda7506fc22c33ac225bf
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2026-01-19 15:07:32 -0500

    added Sigma+Kit.swift

---
+++ ASToolkit/Sigma+Kit.swift
@@ -0,0 +1,24 @@
+//
+//  SigmaKit.swift
+//  ASToolkit
+//
+//  Created by andrzej semeniuk on 2026-01-19.
+//  Copyright © 2026 Andrzej Semeniuk. All rights reserved.
+//
+
+import Foundation
+
+extension Sigma {
+    
+    struct Kit {
+     
+        let values : [Double]
+        
+        init(values: [Double]) {
+            self.values = values
+        }
+        
+        
+    }
+    
+}
