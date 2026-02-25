976f79af853f0bfb1c8d797c0dafdcf56be3e40c
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2026-02-25 08:44:00 -0500

    removal

--- ASToolkit/ExtensionsForSwiftUI.swift
+++ ASToolkit/ExtensionsForSwiftUI.swift
@@ -309,12 +309,6 @@ public extension View {
         
     }
     
-    func viewInScrollViewVertical0(showsIndicators: Bool = false) -> some View {
-        ScrollView(.vertical, showsIndicators: showsIndicators) {
-            self
-        }
-    }
-    
     func viewInScrollViewHorizontal0(showsIndicators: Bool = false) -> some View {
         ScrollView(.horizontal, showsIndicators: showsIndicators) {
             self
